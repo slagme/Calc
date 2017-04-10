d24fc05419dc162328fc4698f9d4892af7fede64
slagme <slawomir.gmerek@gmail.com>
2017-04-10 16:17:18 +0200

    init

---
+++ js/app.js
@@ -0,0 +1,25 @@
+
+var results = [];
+
+var totalString;
+
+var operators1=['+', "-", "/", "*"];
+
+var inputs=[0,1,2,3,4,5,6,7,8,9];
+
+function getValue(input){
+    
+}
+
+function update (){
+    totalString=inputs.join('');
+    $("#steps").html(totalString);
+}
+
+function getTotal(){
+    
+}
+
+btn8.on('click', function(){
+    calcform.display.value+='8';
+});
